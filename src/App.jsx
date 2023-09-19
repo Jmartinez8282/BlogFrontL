52addd857d73f3748f20d5a17083714edadeb2a8
Jose Luis Martinez Lopez <josemartinez8282@gmail.com>
2023-09-18 20:19:47 -0700

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+import { Container, Row, Col } from "react-bootstrap";
+import "./App.css";
+import Dashboard from "./components/Dashboard";
+
+function App() {
+  return (
+    <Container>
+      <Row>
+        <Col>
+          <h1 className="text-center">Our Blog</h1>
+        </Col>
+
+        <Dashboard />
+      </Row>
+    </Container>
+  );
+}
+
+export default App;
