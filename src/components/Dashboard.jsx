0032bcf45b0049d3e9bbf600fb411bfd458228ce
Jose Luis Martinez Lopez <josemartinez8282@gmail.com>
2023-09-19 18:22:41 -0700

    Dashboard visual done

--- src/components/Dashboard.jsx
+++ src/components/Dashboard.jsx
@@ -1,32 +1,38 @@
-import { Button, Container, Form, Modal,Accordion,Row,Col, ListGroup } from "react-bootstrap/";
+import {
+  Button,
+  Container,
+  Form,
+  Modal,
+  Accordion,
+  Row,
+  Col,
+  ListGroup,
+} from "react-bootstrap/";
 import { useState } from "react";
 
 const Dashboard = () => {
   //functions
-  const handleSetTitle = (e) => setBlogTitle(e.target.value)
-  const handleBlogDescription = (e) => setBlogDescription(e.target.value)
-  const handleTag = (e) => setBlogTags(e.target.value)
-  const handleCategory = (e) => setBlogCategory(e.target.value)
-  const handleImage = (e) => setBlogImage(e.target.value)
+  const handleSetTitle = (e) => setBlogTitle(e.target.value);
+  const handleBlogDescription = (e) => setBlogDescription(e.target.value);
+  const handleTag = (e) => setBlogTags(e.target.value);
+  const handleCategory = (e) => setBlogCategory(e.target.value);
+  const handleImage = (e) => setBlogImage(e.target.value);
   const handleClose = () => setShow(false);
   const handleShow = (e) => {
-    setShow(true)
-    if(e.target.textContent == "Add Blog Item")
-    {
+    setShow(true);
+    if (e.target.textContent == "Add Blog Item") {
       setEdit(false);
       setBlogTitle("");
       setBlogDescription("");
-      setBlogCategory("")
-
-    }else
-    {
+      setBlogCategory("");
+    } else {
       setEdit(true);
       setBlogTitle("My Awsome Title");
       setBlogDescription("My Awsome Description");
-      setBlogCategory("Fitness")
+      setBlogCategory("Fitness");
     }
-  }
-  
+  };
+
   //create useStates for our forms
   const [blogTitle, setBlogTitle] = useState("");
   const [blogImage, setBlogImage] = useState("");
@@ -40,10 +46,8 @@ const Dashboard = () => {
       Publisher: "anonymous",
       Date: "01-13-2022",
       Text: "Developing finishing and crossing skills is an important aspect of soccer that can greatly constribute to your player.",
-      Image: 
-            "./assets/Images/3soccerballs.jpg",
-      Published: true
-      
+      Image: "./assets/Images/3soccerballs.jpg",
+      Published: true,
     },
     {
       Id: 2,
@@ -51,9 +55,8 @@ const Dashboard = () => {
       Publisher: "anonymous",
       Date: "01-14-2022",
       Text: "A strong defense is the backbone of any successful soccer team",
-      Image: 
-            "./assets/Images/3soccerballs.jpg",
-      Published: true
+      Image: "./assets/Images/3soccerballs.jpg",
+      Published: true,
     },
     {
       Id: 3,
@@ -61,9 +64,8 @@ const Dashboard = () => {
       Publisher: "anonymous",
       Date: "01-15-2022",
       Text: "Small-sided games create a fast-paced and intense environment.",
-      Image: 
-            "./assets/Images/3soccerballs.jpg",
-      Published: true
+      Image: "./assets/Images/3soccerballs.jpg",
+      Published: true,
     },
     {
       Id: 4,
@@ -71,9 +73,8 @@ const Dashboard = () => {
       Publisher: "anonymous",
       Date: "01-15-2022",
       Text: "One of the best ways to naturally bring out the competitive nature.",
-      Image: 
-            "./assets/Images/3soccerballs.jpg",
-      Published: false
+      Image: "./assets/Images/3soccerballs.jpg",
+      Published: false,
     },
     {
       Id: 5,
@@ -81,16 +82,14 @@ const Dashboard = () => {
       Publisher: "anonymous",
       Date: "01-15-2022",
       Text: "One of the challenges for youth soccer coaches is to make sure their players are always excited to come to practice.",
-      Image: 
-            "./assets/Images/3soccerballs.jpg",
-      Published: false
+      Image: "./assets/Images/3soccerballs.jpg",
+      Published: false,
     },
   ]);
-  
+
   //bools
   const [show, setShow] = useState(false);
   const [edit, setEdit] = useState(false);
-
 
   return (
     <>
@@ -102,22 +101,36 @@ const Dashboard = () => {
         <Modal show={show} onHide={handleClose}>
           <Modal.Header style={{ background: "#2f2f2f" }} closeButton>
             <Modal.Title style={{ background: "#2f2f2f" }}>
-              {edit ? 'Edit' : 'Add'} Blog Item
+              {edit ? "Edit" : "Add"} Blog Item
             </Modal.Title>
           </Modal.Header>
           <Modal.Body style={{ background: "#2f2f2f" }}>
             <Form>
               <Form.Group className="mb-3" controlId="Title">
                 <Form.Label>Title</Form.Label>
-                <Form.Control type="text" placeholder="Enter Title" value={blogTitle} onChange={handleSetTitle} />
+                <Form.Control
+                  type="text"
+                  placeholder="Enter Title"
+                  value={blogTitle}
+                  onChange={handleSetTitle}
+                />
               </Form.Group>
 
               <Form.Group className="mb-3" controlId="Description">
                 <Form.Label>Description</Form.Label>
-                <Form.Control type="text" placeholder="Enter Description" value={blogDescription} onChange={handleBlogDescription} />
+                <Form.Control
+                  type="text"
+                  placeholder="Enter Description"
+                  value={blogDescription}
+                  onChange={handleBlogDescription}
+                />
               </Form.Group>
               <Form.Group className="mb-3" controlId="Category">
-                <Form.Select aria-label="Default select example" value={blogCategory} onChange={handleCategory}>
+                <Form.Select
+                  aria-label="Default select example"
+                  value={blogCategory}
+                  onChange={handleCategory}
+                >
                   <option>Select Category</option>
                   <option value="Food">Food</option>
                   <option value="Fitness">Fitness</option>
@@ -127,14 +140,23 @@ const Dashboard = () => {
               </Form.Group>
               <Form.Group className="mb-3" controlId="Tags">
                 <Form.Label>Tags</Form.Label>
-                <Form.Control type="text" placeholder="Enter Tags" value={blogTags} onChange={handleTag} />
+                <Form.Control
+                  type="text"
+                  placeholder="Enter Tags"
+                  value={blogTags}
+                  onChange={handleTag}
+                />
               </Form.Group>
               <Form.Group className="mb-3" controlId="Image">
                 <Form.Label>Pick an Image</Form.Label>
-                <Form.Control type="file" placeholder="Select Image from file" accept="image/png, image/jpg" value={blogImage} onChange={handleImage} />
+                <Form.Control
+                  type="file"
+                  placeholder="Select Image from file"
+                  accept="image/png, image/jpg"
+                  value={blogImage}
+                  onChange={handleImage}
+                />
               </Form.Group>
-
-              
             </Form>
           </Modal.Body>
           <Modal.Footer style={{ background: "#2f2f2f" }}>
@@ -145,36 +167,59 @@ const Dashboard = () => {
               {edit ? "Save Changes" : "Save"}
             </Button>
             <Button variant="outline-primary" onClick={handleClose}>
-              {edit ? "Save Changes" : "Save"} and Publish 
+              {edit ? "Save Changes" : "Save"} and Publish
             </Button>
           </Modal.Footer>
         </Modal>
 
-        <Button variant="outline-primary" onClick={handleShow}>Edit Blog Item</Button>
+        <Button variant="outline-primary" onClick={handleShow}>
+          Edit Blog Item
+        </Button>
 
         <Row>
           <Col>
-          
-          <Accordion defaultActiveKey={['0']} alwaysOpen>
-      <Accordion.Item eventKey="0">
-        <Accordion.Header>Accordion Item #1</Accordion.Header>
-        <Accordion.Body style={{backgroundColor: "#3f3f3f", color: "azure"}}>
-      {blogItems.map((item) => item.Published ? <ListGroup>{item.Title}</ListGroup> : null)}
-        </Accordion.Body>
-      </Accordion.Item>
-      <Accordion.Item eventKey="1">
-        <Accordion.Header>Accordion Item #2</Accordion.Header>
-        <Accordion.Body style={{backgroundColor: "#3f3f3f", color: "azure"}}>
-        {blogItems.map((item) => !item.Published ? <ListGroup>{item.Title}</ListGroup> : null)}
-        </Accordion.Body>
-      </Accordion.Item>
-    </Accordion>
-          
+            <Accordion defaultActiveKey={["0", "1"]} alwaysOpen>
+              <Accordion.Item eventKey="0">
+                <Accordion.Header>Publish</Accordion.Header>
+                <Accordion.Body
+                  style={{ backgroundColor: "#3f3f3f", color: "azure" }}
+                >
+                  {blogItems.map((item) =>
+                    item.Published ? (
+                      <ListGroup key={item.Title}>
+                        {item.Title}
+                        <Col className="d-flex justify-content-end">
+                          <Button variant="outline-danger mx-2">Delete</Button>
+                          <Button variant="outline-info mx-2" >Edit</Button>
+                          <Button variant="outline-primary mx-2">Publish</Button>
+                        </Col>
+                      </ListGroup>
+                    ) : null
+                  )}
+                </Accordion.Body>
+              </Accordion.Item>
+              <Accordion.Item eventKey="1">
+                <Accordion.Header>Unpublished</Accordion.Header>
+                <Accordion.Body
+                  style={{ backgroundColor: "#3f3f3f", color: "azure" }}
+                >
+                  {blogItems.map((item) =>
+                    !item.Published ? (
+                      <ListGroup key={item.Title}>{item.Title}
+                      <Col className="d-flex justify-content-end">
+                          <Button variant="outline-danger mx-2">Delete</Button>
+                          <Button variant="outline-info mx-2" >Edit</Button>
+                          <Button variant="outline-primary mx-2">Upublish</Button>
+                        </Col>
+                      
+                      </ListGroup>
+                    ) : null
+                  )}
+                </Accordion.Body>
+              </Accordion.Item>
+            </Accordion>
           </Col>
         </Row>
-
-
-
       </Container>
     </>
   );
